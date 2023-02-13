67bcad43e0c3c2dae2da463ab1308c77d3e361b8
Nirob Hasan <nirob427417@gmail.com>
2023-02-13 14:19:07 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+// require('crypto').randomBytes(64).toString('hex')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+app.listen(port, () => {
+    console.log(` server running on ${port}`);
+})
